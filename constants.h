9e29c9897d92e9d4965ba8d17c2e941a374f61a9
Ibrahim Sha'ath <ibrahimshaath@gmail.com>
2013-07-11 14:55:46 +0100

    Style

--- constants.h
+++ constants.h
@@ -31,18 +31,18 @@
 namespace KeyFinder {
 
   enum key_t {
-    A_MAJOR,       A_MINOR,
-    B_FLAT_MAJOR,	 B_FLAT_MINOR,
-    B_MAJOR,       B_MINOR,
-    C_MAJOR,       C_MINOR,
-    D_FLAT_MAJOR,	 D_FLAT_MINOR,
-    D_MAJOR,       D_MINOR,
-    E_FLAT_MAJOR,	 E_FLAT_MINOR,
-    E_MAJOR,       E_MINOR,
-    F_MAJOR,       F_MINOR,
-    G_FLAT_MAJOR,	 G_FLAT_MINOR,
-    G_MAJOR,       G_MINOR,
-    A_FLAT_MAJOR,	 A_FLAT_MINOR,
+    A_MAJOR,      A_MINOR,
+    B_FLAT_MAJOR, B_FLAT_MINOR,
+    B_MAJOR,      B_MINOR,
+    C_MAJOR,      C_MINOR,
+    D_FLAT_MAJOR, D_FLAT_MINOR,
+    D_MAJOR,      D_MINOR,
+    E_FLAT_MAJOR, E_FLAT_MINOR,
+    E_MAJOR,      E_MINOR,
+    F_MAJOR,      F_MINOR,
+    G_FLAT_MAJOR, G_FLAT_MINOR,
+    G_MAJOR,      G_MINOR,
+    A_FLAT_MAJOR, A_FLAT_MINOR,
     SILENCE
   };
 
