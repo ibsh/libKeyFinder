774973a9699a15dfb45e930f089aa485160b1a57
Ibrahim Sha'ath <ibrahimshaath@gmail.com>
2014-10-21 22:10:37 +0100

    consts

--- constants.h
+++ constants.h
@@ -60,19 +60,31 @@ static const unsigned int octaves = 6;
 namespace KeyFinder {
 
   enum key_t {
-    A_MAJOR,      A_MINOR,
-    B_FLAT_MAJOR, B_FLAT_MINOR,
-    B_MAJOR,      B_MINOR,
-    C_MAJOR,      C_MINOR,
-    D_FLAT_MAJOR, D_FLAT_MINOR,
-    D_MAJOR,      D_MINOR,
-    E_FLAT_MAJOR, E_FLAT_MINOR,
-    E_MAJOR,      E_MINOR,
-    F_MAJOR,      F_MINOR,
-    G_FLAT_MAJOR, G_FLAT_MINOR,
-    G_MAJOR,      G_MINOR,
-    A_FLAT_MAJOR, A_FLAT_MINOR,
-    SILENCE
+    A_MAJOR = 0,
+    A_MINOR,
+    B_FLAT_MAJOR,
+    B_FLAT_MINOR,
+    B_MAJOR,
+    B_MINOR = 5,
+    C_MAJOR,
+    C_MINOR,
+    D_FLAT_MAJOR,
+    D_FLAT_MINOR,
+    D_MAJOR = 10,
+    D_MINOR,
+    E_FLAT_MAJOR,
+    E_FLAT_MINOR,
+    E_MAJOR,
+    E_MINOR = 15,
+    F_MAJOR,
+    F_MINOR,
+    G_FLAT_MAJOR,
+    G_FLAT_MINOR,
+    G_MAJOR = 20,
+    G_MINOR,
+    A_FLAT_MAJOR,
+    A_FLAT_MINOR,
+    SILENCE = 24
   };
 
   enum temporal_window_t {
